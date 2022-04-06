c304a91aebd6af40ab96a791819e30a3b75287db
ChrisShocker <fallenchris@live.com>
2022-04-06 15:19:47 -0700

    Section 2: 13 Properties(normal and auto implemented)

---
+++ Section2/Properties/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Properties
+{
+    public class Person
+    {
+        public DateTime Birthdate { get; private set; }
+        public string Name { get; private set; }
+
+        public Person(DateTime birthdate, String name)
+        {
+            this.Birthdate = birthdate;
+            this.Name = name;
+        }
+
+        //Birthdate should only set once, so private and no setter method
+        //We can calculate their age based on current time - their birthDate
+        public int Age
+        {
+            get
+            {
+                var timespan = DateTime.Today - Birthdate;
+                var years = timespan.Days / 365;
+                return years;
+            }
+        }
+    }
+}
---
+++ Section2/Properties/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Properties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person(new DateTime(1876,1,2), "Hello");
+            Console.WriteLine("Name: {0} Age: {1}",person.Name, person.Age);
+        }
+    }
+}
