2079620af7d473aa310d6afa7abf95c100861b83
ChrisShocker <fallenchris@live.com>
2022-04-06 16:07:58 -0700

    Section 2: 14 Indexers(Dictionary and using indexers)

---
+++ Section2/Indexer/HttpCookie.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace Indexer
+{
+    public class HttpCookie
+    {
+        private readonly Dictionary<string, string> _dictonary;
+
+        public HttpCookie()
+        {
+            this._dictonary = new Dictionary<string, string>();
+        }
+
+        public string this[string key]
+        {
+            get
+            {
+                return this._dictonary[key];
+
+            }
+            set
+            {
+                this._dictonary[key] = value;
+            }
+        }
+    }
+}
---
+++ Section2/Indexer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Indexer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var cookie = new HttpCookie();
+            cookie["name"] = "Jake";
+
+            Console.WriteLine(cookie["name"]);
+        }
+    }
+}
