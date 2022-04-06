689845c3430fe427dfddf99a49fe13aa8770c36c
ChrisShocker <fallenchris@live.com>
2022-04-06 14:43:18 -0700

    Section 2: 12 Access Modifiers(private members, getters and setters)

---
+++ Section2/Access-Modifiers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Access_Modifiers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.setName("John");
+            person.setBirthdate(new DateTime(2022,12,20));
+            Console.WriteLine(person.getName());
+            Console.WriteLine(person.getBirthDate());
+        }
+    }
+
+    public class Person
+    {
+        private string _name;
+        private DateTime _birthdate;
+
+        public void setBirthdate(DateTime birthdate)
+        {
+            this._birthdate = birthdate;
+        }
+
+        public void setName(String name)
+        {
+            this._name = name;
+        }
+
+        public String getName()
+        {
+            return this._name;
+        }
+        public DateTime getBirthDate()
+        {
+            return this._birthdate;
+        }
+    }
+}
