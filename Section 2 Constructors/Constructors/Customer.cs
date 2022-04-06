dbc0ecdb25e6d199084344a26196fbf9a2dc7f2a
ChrisShocker <fallenchris@live.com>
2022-04-06 12:54:04 -0700

    Section 2: 8 Constructors

---
+++ Section 2 Constructors/Constructors/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Constructors
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+        public List<Order> Orders;
+
+        public Customer()
+        {
+            Orders = new List<Order>();
+        }
+
+        public Customer(string name):this()
+        {
+            this.Name = name;
+        }
+
+        public Customer(int id):this()
+        {
+            this.Id = id;
+        }
+
+        public Customer(string name, int id):this(id)
+        {
+            this.Name = name;
+            this.Id = id;
+        }
+    }
+}
---
+++ Section 2 Constructors/Constructors/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Constructors
+{
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer("Jake", 1);
+            Console.WriteLine(customer.Name + " Id: " + customer.Id);
+            var order = new Order();
+            customer.Orders.Add(order);
+
+        }
+    }
+}
