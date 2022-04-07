b39c51780a3d948548dc234e9a74b4cb71d340c9
ChrisShocker <fallenchris@live.com>
2022-04-06 18:29:48 -0700

    Section 2: 16 Changed Stopwatch folder name and added description

---
+++ Section2/StopWatch Exercise/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace StopWatch
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Design a class called Stopwatch. The job of this class is to simulate a stopwatch. 
+             * It should provide two methods: Start and Stop. We call the start method first, and the stop method next. 
+             * Then we ask the stopwatch about the duration between start and stop. Duration should be a value in TimeSpan. 
+             * Display the duration on the console.
+             * 
+             * We should also be able to use a stopwatch multiple times. So we may start and stop it and then start and stop it again.
+             * Make sure the duration value each time is calculated properly.
+             *
+             *We should not be able to start a stopwatch twice in a row (because that may overwrite the initialstart time). 
+             *So the class should throw an InvalidOperationException if its started twice.
+             */
+
+
+            var watch = new Watch();
+            watch.Start();
+            watch.Stop();
+            watch.Start();
+            watch.Stop();
+            watch.Start();
+            watch.Stop();
+            watch.Start();
+            watch.Stop();
+        }
+    }
+}
