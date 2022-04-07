3b4bd2e00cf3fa8f2616c14caa4ff21b923cc251
ChrisShocker <fallenchris@live.com>
2022-04-07 16:27:09 -0700

    Section 5: 29 Abstract Classes and Members Inserted comment to outline Abstract effects

--- Section5-Polymorphism/Abstract_Classes_and_Members/Program.cs
+++ Section5-Polymorphism/Abstract_Classes_and_Members/Program.cs
@@ -1,10 +1,14 @@
 ﻿namespace Abstract_Classes_and_Members
 {
+    //Abstract classes and methods allow us to enforce how
+    //our classes are used by forcing abstract methods to be 
+    //implemented providing a commonality between objects
     internal class Program
     {
         static void Main(string[] args)
         {
-            var circle = new Circle();
+            var circle = new Circle();:wa
+
             circle.Draw();
 
             var rectangle = new Rectangle();
