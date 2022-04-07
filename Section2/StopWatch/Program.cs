a662b4ede32be0d937645f99700d65d73f64c6bc
ChrisShocker <fallenchris@live.com>
2022-04-06 17:53:10 -0700

    Section 2: 16 Completed StopWatch exercise

---
+++ Section2/StopWatch/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StopWatch
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            var watch = new Watch();
+            watch.Start();
+            watch.Stop();
+            watch.Start();
+            watch.Stop();
+            watch.Start();
+            watch.Stop();
+            watch.Start();
+            watch.Stop();
+        }
+    }
+}
---
+++ Section2/StopWatch/Watch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace StopWatch
+{
+    internal partial class Program
+    {
+        public class Watch
+        {
+            private TimeSpan _totalTime;
+            private bool _isRunning;
+
+            public TimeSpan Start()
+            {
+                if (!_isRunning)
+                {
+                    DateTime _startTime = DateTime.Now;
+                    Console.WriteLine("Stop watch started, the current timer is: {0}", _totalTime);
+                    _isRunning = true;
+                    return _totalTime += (DateTime.Now - _startTime);
+                }
+                else
+                    throw new InvalidOperationException("Can't start twice");
+            }
+
+
+            public TimeSpan Stop()
+            {
+                _isRunning = false;
+                Console.WriteLine("Stopwatch stopped, the current timer is: {0} ", _totalTime);
+                return _totalTime;
+            }
+
+        }
+    }
+}
