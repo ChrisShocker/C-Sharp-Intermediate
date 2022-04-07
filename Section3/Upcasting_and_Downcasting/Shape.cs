6c16d8dc76d5be81b690461e48bcc4599a6ba5e5
ChrisShocker <fallenchris@live.com>
2022-04-07 13:55:47 -0700

    Section 4: Upcasting and Downcasting Examples

---
+++ Section3/Upcasting_and_Downcasting/Shape.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Upcasting_and_Downcasting
+{
+    public class Shape
+    {
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public void Draw()
+        {
+            //Upcasting
+            Square square = new Square();
+            //Base class set to a derived class
+            Shape shape = square;
+
+            //Text and shape are references to the same obj in memory
+            //Changing either one affects the same memory
+            square.Width = 200;
+            shape.Width = 100;
+            //Now square .Width and shape.Width = 100
+
+
+            //Unsafe type list
+            //any object type can be added without any conversion
+            //Array elements are being converted to the target type
+            var list = new ArrayList();
+            list.Add(1);
+            list.Add("help");
+
+            //Generic type list
+            //Only the specified type can be added unless converted
+            var genericList = new List<Shape>();
+
+
+            //shapeD is of type Square
+            Shape shapeD = new Square();
+
+            //Downcast object and cast to access class properties
+            //Convert ShapeD is of type sqaure
+            Square squareD = (Square) shapeD;
+
+
+        }
+
+
+
+    }
+}
---
+++ Section3/Upcasting_and_Downcasting/Square.cs
@@ -0,0 +1,9 @@
+﻿namespace Upcasting_and_Downcasting
+{
+    public class Square : Shape
+    {
+        public int FontSize { get; set; }
+        public int FontName { get; set; }
+    }
+
+}
