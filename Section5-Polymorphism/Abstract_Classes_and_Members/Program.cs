d2f238a37d48ee372ed2baa3521001067ed85837
ChrisShocker <fallenchris@live.com>
2022-04-07 16:31:08 -0700

    Section 5: 29 Abstact Classes and Members Fixed compile erros and comments

--- Section5-Polymorphism/Abstract_Classes_and_Members/Program.cs
+++ Section5-Polymorphism/Abstract_Classes_and_Members/Program.cs
@@ -1,19 +1,25 @@
 ﻿namespace Abstract_Classes_and_Members
 {
-    //Abstract classes and methods allow us to enforce how
-    //our classes are used by forcing abstract methods to be 
-    //implemented providing a commonality between objects
+    /*
+     * Abstract classes and methods allow us to enforce how
+     * implemented providing a commonality between objects
+     * our classes are used by forcing abstract methods to be 
+     * implemented providing a commonality between objects
+     */
     internal class Program
     {
         static void Main(string[] args)
         {
-            var circle = new Circle();:wa
+
+            // Shapes class is abstract so we can't create a Shapes object
+            //var shape = new Shapes();
+
+            var circle = new Circle();
 
             circle.Draw();
 
             var rectangle = new Rectangle();
             rectangle.Draw();
-
         }
     }
 }
