c05dd7d515dd62e68f548b6fff1d6a5bab21a533
ChrisShocker <fallenchris@live.com>
2022-04-11 13:42:00 -0700

    Section 6: 35 Interfaces and Extensibility Started section

---
+++ Section6-Interfaces/Interfaces and Extensibility/DbMigrator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Interfaces_and_Extensibility
+{
+    public class DbMigrator
+    {
+        public void Migrate()
+        {
+            Console.WriteLine("Migration started at {0}", DateTime.Now);
+        }
+    }
+}
