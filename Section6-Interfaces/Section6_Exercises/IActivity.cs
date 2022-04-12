7ef70f643ade26d00612dbeb723f5ad586416039
ChrisShocker <fallenchris@live.com>
2022-04-12 13:01:08 -0700

    Section 6: 39 Exercises Started Exercise

---
+++ Section6-Interfaces/Section6_Exercises/IActivity.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+
+namespace Section6_Exercises
+{
+    public interface IActivity
+    {
+        void Execute();
+    }
+
+    public class UploadVideo : IActivity
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Uploaded a video");
+        }
+       
+    }
+
+    public class DownloadVideo : IActivity
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Downloaded a video");
+        }
+    }
+
+    public class Call : IActivity
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Called someone");
+        }
+    }
+    public class SendEmail : IActivity
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Sent email to someone");
+        }
+    }
+    public class UpdateStatus : IActivity
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Status updated");
+        }
+    }
+}
